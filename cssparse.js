54b2ac9b1557403d5bc7e8c8069b04ec485cdd6d
Anatoly Lapshin <holywarez@gmail.com>
2013-10-01 18:24:59 +0400

    Update cssparse.js

--- cssparse.js
+++ cssparse.js
@@ -10,7 +10,7 @@ function RGBColor(color_string)
 
     // strip any leading #
     if (color_string.charAt(0) == '#') { // remove # if any
-        color_string = color_string.substr(1,6);
+      color_string = color_string.substr(1,6);
     }
 
     color_string = color_string.replace(/ /g,'');
@@ -163,10 +163,11 @@ function RGBColor(color_string)
         yellow: 'ffff00',
         yellowgreen: '9acd32'
     };
+    
     for (var key in simple_colors) {
-        if (color_string == key) {
-            color_string = simple_colors[key];
-        }
+      if (color_string == key) {
+        color_string = simple_colors[key];
+      }
     }
     // emd of simple type-in colors
 
@@ -176,22 +177,22 @@ function RGBColor(color_string)
             re: /^rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)$/,
             example: ['rgb(123, 234, 45)', 'rgb(255,234,245)'],
             process: function (bits){
-                return [
-                    parseInt(bits[1]),
-                    parseInt(bits[2]),
-                    parseInt(bits[3])
-                ];
+              return [
+                  parseInt(bits[1]),
+                  parseInt(bits[2]),
+                  parseInt(bits[3])
+              ];
             }
         },
         {
             re: /^(\w{2})(\w{2})(\w{2})$/,
             example: ['#00ff00', '336699'],
             process: function (bits){
-                return [
-                    parseInt(bits[1], 16),
-                    parseInt(bits[2], 16),
-                    parseInt(bits[3], 16)
-                ];
+              return [
+                  parseInt(bits[1], 16),
+                  parseInt(bits[2], 16),
+                  parseInt(bits[3], 16)
+              ];
             }
         },
         {
@@ -209,16 +210,16 @@ function RGBColor(color_string)
 
     // search through the definitions to find a match
     for (var i = 0; i < color_defs.length; i++) {
-        var re = color_defs[i].re;
-        var processor = color_defs[i].process;
-        var bits = re.exec(color_string);
-        if (bits) {
-            channels = processor(bits);
-            this.r = channels[0];
-            this.g = channels[1];
-            this.b = channels[2];
-            this.ok = true;
-        }
+      var re = color_defs[i].re;
+      var processor = color_defs[i].process;
+      var bits = re.exec(color_string);
+      if (bits) {
+        channels = processor(bits);
+        this.r = channels[0];
+        this.g = channels[1];
+        this.b = channels[2];
+        this.ok = true;
+      }
 
     }
 
@@ -226,63 +227,35 @@ function RGBColor(color_string)
     this.r = (this.r < 0 || isNaN(this.r)) ? 0 : ((this.r > 255) ? 255 : this.r);
     this.g = (this.g < 0 || isNaN(this.g)) ? 0 : ((this.g > 255) ? 255 : this.g);
     this.b = (this.b < 0 || isNaN(this.b)) ? 0 : ((this.b > 255) ? 255 : this.b);
+    
+    
 
     // some getters
     this.toRGB = function () {
-        return 'rgb(' + this.r + ', ' + this.g + ', ' + this.b + ')';
-    }
+      return 'rgb(' + this.r + ', ' + this.g + ', ' + this.b + ')';
+    };
+    
     this.toHex = function () {
-        var r = this.r.toString(16);
-        var g = this.g.toString(16);
-        var b = this.b.toString(16);
-        if (r.length == 1) r = '0' + r;
-        if (g.length == 1) g = '0' + g;
-        if (b.length == 1) b = '0' + b;
-        return '#' + r + g + b;
-    }
-
-    // help
-    this.getHelpXML = function () {
-
-        var examples = new Array();
-        // add regexps
-        for (var i = 0; i < color_defs.length; i++) {
-            var example = color_defs[i].example;
-            for (var j = 0; j < example.length; j++) {
-                examples[examples.length] = example[j];
-            }
-        }
-        // add type-in colors
-        for (var sc in simple_colors) {
-            examples[examples.length] = sc;
-        }
-
-        var xml = document.createElement('ul');
-        xml.setAttribute('id', 'rgbcolor-examples');
-        for (var i = 0; i < examples.length; i++) {
-            try {
-                var list_item = document.createElement('li');
-                var list_color = new RGBColor(examples[i]);
-                var example_div = document.createElement('div');
-                example_div.style.cssText =
-                        'margin: 3px; '
-                        + 'border: 1px solid black; '
-                        + 'background:' + list_color.toHex() + '; '
-                        + 'color:' + list_color.toHex()
-                ;
-                example_div.appendChild(document.createTextNode('test'));
-                var list_item_value = document.createTextNode(
-                    ' ' + examples[i] + ' -> ' + list_color.toRGB() + ' -> ' + list_color.toHex()
-                );
-                list_item.appendChild(example_div);
-                list_item.appendChild(list_item_value);
-                xml.appendChild(list_item);
-
-            } catch(e){}
-        }
-        return xml;
-
-    }
+      var r = this.r.toString(16);
+      var g = this.g.toString(16);
+      var b = this.b.toString(16);
+      if (r.length == 1) r = '0' + r;
+      if (g.length == 1) g = '0' + g;
+      if (b.length == 1) b = '0' + b;
+      return '#' + r + g + b;
+    };
+    
+    // http://alienryderflex.com/hsp.html
+    this.brightness = function() {
+      return Math.sqrt(
+         this.r * this.r * .299 +
+         this.g * this.g * .587 +
+         this.b * this.b * .114);   
+    };
+    
+    this.textColor = function() {
+      return (this.ok && this.brightness() < 130) ? "#FFFFFF" : "#000000";
+    };
 
 }
 
